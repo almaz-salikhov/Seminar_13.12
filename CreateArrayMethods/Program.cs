c4fd5c5a8b8d263c4f77ee02f1b70d08738da2c5
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-13 22:26:34 +0200

    Seminar' task solved

---
+++ CreateArrayMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿// Создание массива с использованием методов
+
+// 1. Первый способ
+int[] CreateArray (int length) // создаем массив, вводя размер массива
+{
+    int[] array = new int[length];
+    Random rnd = new Random(); // вынесен отдельно, чтобы не выделять память под это
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next();
+    }
+    return array; // возвращает массив
+}
+
+int[] arr1 = CreateArray(5); // вызов метода
+
+
+// 2. Второй способ
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next();
+    }
+}
+
+int[] arr2 = new int[5]; // создание массива размером 5 ячеек
+FillArray(arr2); // вызов метода
---
+++ task1/Program.cs
@@ -0,0 +1,55 @@
+﻿// Программа заполняет массив четными цифрами 
+// случайно сгенерированного от 1 000 до 1000 000 целого числа
+
+// получить случайное число 
+// заполнение массива с проверкой на четность
+
+// int[] FillArrayWithEven(int val, int arr) // Метод while 
+// {
+//     int i = 0; // иначе в цикле ш постоянно будет обнуляться 
+//     while (val > 0)
+//     {
+//         int digit = val % 10;
+//         if(digit % 2 == 0)
+//         {
+//             // YES
+//             arr[i] = digit;
+//             i++;
+//         }
+//         val = val / 10;
+//     }
+// }
+
+int GetLengthOfNumber(int val)
+{
+    int count = 0; // количество цифр
+    while(val > 0) // сначала определись с действием, потом услови 
+    {   
+        val = val / 10;
+        count++;
+    }
+    return count;
+}
+
+int[] FillArrayWithEven(int val, int length) // создаем массив, вводя размер массива
+{
+    int[] arr = new int[length];
+    int digit = 0;
+    for (int i = 0; i < length; i++)
+    {
+        digit = val % 10;
+        if(digit % 2 == 0)
+        {
+            arr[i] = digit;
+            Console.Write(arr[i] + " ");
+        }
+        val = val / 10;
+    }
+    return arr; // возвращает массив
+
+}
+
+int number = new Random().Next(1000, 1000000);
+Console.WriteLine("Случайное число: " + number);
+Console.Write("Четные цифры введенного числа: ");
+int[] array = FillArrayWithEven(number, GetLengthOfNumber(number));
